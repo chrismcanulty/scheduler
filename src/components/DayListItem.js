3a9b44f8f24665742008c44ffcee3472de3fda53
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-10-05 21:03:45 -0700

    build out interviewer and day list and list item components, starting to build out the appointment components

---
+++ src/components/DayListItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0
+  });
+  const spotsRemaining = () => {
+    let returnMessage = ""
+    if (props.spots > 1) {
+      returnMessage = `${props.spots} spots remaining`;
+    }
+    else if (props.spots === 1) {
+      returnMessage = `${props.spots} spot remaining`;
+    }
+    else if (props.spots === 0) {
+      returnMessage = "no spots remaining";
+    }
+    return returnMessage;
+  }
+  return (
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{spotsRemaining()}</h3>
+    </li>
+  );
+}
---
+++ src/components/InterviewerList.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss"
+
+export default function InterviewerList(props) {
+  const interviewersList = props.interviewers.map((person) => {
+    return (
+      <InterviewerListItem
+        key={person.id}
+        name={person.name}
+        avatar={person.avatar}
+        selected={person.id === props.value}
+        setInterviewer={() => props.onChange(person.id)}
+      />
+    )
+  })
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewersList}</ul>
+    </section>
+  )
+}
